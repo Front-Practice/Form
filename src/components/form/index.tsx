a2cc0478e481062cbb8a441e89074a6869a1e923
Muhammad <muhammmmadazizekubov@gmail.com>
2025-07-24 10:59:53 +0500

    start

---
+++ src/components/form/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className='flex items-center justify-center'>
+      <div className='w-[350px] h-[600px] bg-black mt-[70px]'></div>
+    </div>
+  )
+}
+
+export default Form
